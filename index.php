dcb6429a2fa0a2849aac8b488334a0160d8c7ac4
PhoebeMeow <302303913@student.rocmondriaan.nl>
2024-01-15 10:28:14 +0100

    clicker game
    this is the base of the clickergame

---
+++ index.php
@@ -0,0 +1,37 @@
+<?php
+session_start();
+
+if(!isset($_POST['click'])) {
+    $_SESSION['clicks'] += 1;
+}
+if(!isset($_SESSION['clicks'])) {
+    $_SESSION['clicks'] = 0;
+}
+
+
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<body>
+<form method="post">
+    <div class="position-absolute top-50 start-50 translate-middle">
+        <input type="image" src="images/PHP_logo.png" height="150" width="150" name="click">
+    </div>
+</form>
+
+<p id="click-count">Clicks: <?php echo $_SESSION['clicks']; ?></p>
+</body>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+        crossorigin="anonymous"></script>
+</html>
